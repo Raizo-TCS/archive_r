7a47a59d985deac558a416b82fdfb0c9f2fb8a79
Raizo T.C.S <raizo.tcs@gmail.com>
2025-12-01 17:11:27 +0900

    Adjust macOS deployment targets and Windows mode type

--- include/archive_r/platform_compat.h
+++ include/archive_r/platform_compat.h
@@ -5,8 +5,15 @@
 
 #include <sys/types.h>
 
-#if defined(_WIN32) && !defined(_SSIZE_T_DEFINED)
-#  include <BaseTsd.h>
+#if defined(_WIN32)
+#  include <sys/stat.h>
+#  if !defined(_SSIZE_T_DEFINED)
+#    include <BaseTsd.h>
 using ssize_t = SSIZE_T;
-#  define _SSIZE_T_DEFINED
+#    define _SSIZE_T_DEFINED
+#  endif
+#  if !defined(_MODE_T_DEFINED)
+using mode_t = unsigned short; // MSVC does not expose POSIX mode_t by default
+#    define _MODE_T_DEFINED
+#  endif
 #endif
